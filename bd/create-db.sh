1c97a9b986af6dc9e59d2d45b80afc6aa30ae989
Anis <nouurianis@gmail.com>
2023-06-21 15:17:55 +0100

    add dockerfile  for angular

---
+++ bd/create-db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CYAN='\033[1;36m'
+NO_COLOR='\033[0m'
+LABEL="create-db"
+printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
+
+# MySQL/MariaDB container details
+CONTAINER_NAME="mysql"
+DB_USER="root"
+DB_PASSWORD="root"
+DB_NAME="recrutement"
+
+# Path to the SQL dump file
+SQL_DUMP_FILE="/workspaces/E-recrutement/bd/job_db.sql"
+
+# Import the SQL dump file using docker exec
+docker exec -i "$CONTAINER_NAME" mysql -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" < "$SQL_DUMP_FILE"
